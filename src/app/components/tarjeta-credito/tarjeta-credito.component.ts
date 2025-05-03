b8177a031d95ed6203f5ed55be67d190dc45c886
Judit Lustres <judit.lustres@cbl-electronics.ch>
2025-05-03 11:47:48 +0200

    Subida inicial de archivos

---
+++ src/app/components/tarjeta-credito/tarjeta-credito.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tarjeta-credito',
+  imports: [],
+  templateUrl: './tarjeta-credito.component.html',
+  styleUrl: './tarjeta-credito.component.css'
+})
+export class TarjetaCreditoComponent {
+
+}
